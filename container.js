fb1a029a17d178ff29cc8531763b9df15ff1d57e
Lars Wallin <larscwallin@gmail.com>
2013-11-20 12:06:36 +0100

    Create container.js

---
+++ container.js
@@ -0,0 +1,23 @@
+/*
+	This document is a purposed structure for JSON EPUB
+	It is created and maintained by Lars C Wallin (@larscwallin)
+
+  This is the OCF container document as specified in http://www.idpf.org/epub/30/spec/epub30-ocf.html
+
+*/
+
+
+var ocf = {
+	xmlns:{
+			ocf:"urn:oasis:names:tc:opendocument:xmlns:container"
+	},
+	container : {		
+		version:"1.0",
+		rootfile :[
+			{
+				full_path:"EPUB/package.opf.js", 
+				media_type:"application/oebps-package+javascript"
+			}
+		]
+	}
+};
